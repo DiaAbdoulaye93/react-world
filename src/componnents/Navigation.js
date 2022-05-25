a064b756c59fdaea6b38d2673612d6cb41a3f069
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-05-25 10:48:07 +0000

    nav stylisation and logo

--- src/componnents/Navigation.js
+++ src/componnents/Navigation.js
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div className="navigation">
-            <NavLink exact to ="/">
+        <div className="navigation" >
+            <NavLink exact to ="/" activeclassname="nav-active">
                 Accueil
             </NavLink>
-            <NavLink exact to ="/a-propos">
+            <NavLink exact to ="/a-propos" activeclassname="nav-active ">
                 À propos
             </NavLink>
         </div>

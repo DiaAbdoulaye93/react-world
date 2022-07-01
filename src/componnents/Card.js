2bca55a9dd284422f663cafbf972ddfc6bace82a
Abdoulaye <diaabdoulayedjibril@gmail.com>
2022-07-01 17:00:51 +0000

    card styling

--- src/componnents/Card.js
+++ src/componnents/Card.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 const Card = (props) => {
 
-    const country = props;
-    console.log(country);
-    return (
-     <li className='card'>
-        <img src={country.flag} alt="flag"></img>
-     </li>
-    );
+   const country = props.country;
+   console.log(country);
+   return (
+      <li className='card'>
+         <img src={country.flags.png} alt="flag" />
+         <div className='data-container'>
+            <ul>
+
+               <li>{country.name.common}</li>
+               <li>{country.name.common}</li>
+               <li>{country.area}</li>
+            </ul>
+         </div>
+      </li>
+   );
 };
 
 export default Card;
